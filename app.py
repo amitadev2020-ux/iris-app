73c4c34995e89f8bfcd3319040f0acf6916fdf67
amitadev2020-ux <amita.dev2020@gmail.com>
2026-07-07 18:48:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ petal_length = st.slider("Petal length (cm)", 1.0,7.0,4.0)
 petal_width = st.slider("Petal width (cm)" , 0.1,2.5,1.2)
 
 features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
-if st.button("predict")
+if st.button("predict"):
 prediction = model.predict(features)
 class_name = ['Sentos','Versicolor','virginica']
 st.success("The predicted Iris species is: {class_name[prediction[0]]}")
